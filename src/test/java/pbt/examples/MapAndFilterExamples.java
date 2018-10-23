5c707acc6fc92b005d3da06a33bd256b549a0ed1
Johannes Link <business@johanneslink.net>
2018-10-23 15:32:22 +0200

    Added map and filter examples

---
+++ src/test/java/pbt/examples/MapAndFilterExamples.java
@@ -0,0 +1,28 @@
+package pbt.examples;
+
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.*;
+
+class MapAndFilterExamples {
+
+	@Property
+	boolean evenNumbersAreEven(@ForAll("even") int anInt) {
+		return anInt % 2 == 0;
+	}
+
+	@Provide
+	Arbitrary<Integer> even() {
+		return Arbitraries.integers().filter(i -> i % 2 == 0);
+	}
+
+	@Provide
+	Arbitrary<Integer> evenUpTo10000() {
+		return Arbitraries.integers().between(1, 10000).filter(i -> i % 2 == 0);
+	}
+
+	@Provide
+	Arbitrary<Integer> _evenUpTo10000() {
+		return Arbitraries.integers().between(1, 5000).map(i -> i * 2);
+	}
+
+}
