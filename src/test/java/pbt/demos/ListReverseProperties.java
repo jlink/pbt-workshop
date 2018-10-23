6d06d59710d6b2c9bca7e12de481412f631b73ac
Johannes Link <business@johanneslink.net>
2018-10-23 14:55:52 +0200

    Added reverse list demo

---
+++ src/test/java/pbt/demos/ListReverseProperties.java
@@ -0,0 +1,57 @@
+package pbt.demos;
+
+import java.util.*;
+
+import org.assertj.core.api.*;
+
+import net.jqwik.api.*;
+
+class ListReverseProperties {
+
+	private <T> List<T> reverse(List<T> original) {
+		List<T> clone = new ArrayList<>(original);
+		// Should produce failing properties:
+		// List<T> clone = new ArrayList<>(new HashSet<>(original));
+		Collections.reverse(clone);
+		return clone;
+	}
+
+	@Property
+	boolean sizeRemainsTheSame(@ForAll List<Integer> original) {
+		List<Integer> reversed = reverse(original);
+		return original.size() == reversed.size();
+	}
+
+	@Property
+	void allElementsStay(@ForAll List<Integer> original) {
+		List<Integer> reversed = reverse(original);
+		Assertions.assertThat(original).allMatch(element -> reversed.contains(element));
+	}
+
+	@Property
+	boolean reverseMakesFirstElementLast(@ForAll List<Integer> original) {
+		Assume.that(original.size() > 2);
+		Integer lastReversed = reverse(original).get(original.size() - 1);
+		return original.get(0).equals(lastReversed);
+	}
+
+	@Property
+	boolean reverseTwiceIsOriginal(@ForAll List<Integer> original) {
+		return reverse(reverse(original)).equals(original);
+	}
+
+	// Using wildcards and type variables in properties
+
+	@Property
+	@Report(Reporting.GENERATED)
+	boolean reverseWithWildcardType(@ForAll List<?> original) {
+		return reverse(reverse(original)).equals(original);
+	}
+
+	@Property
+	@Report(Reporting.GENERATED)
+	<T> boolean reverseWithTypeVariable(@ForAll List<T> original) {
+		return reverse(reverse(original)).equals(original);
+	}
+
+}
---
+++ src/test/java/pbt/demos/ListReverseTests.java
@@ -0,0 +1,50 @@
+package pbt.demos;
+
+import java.util.ArrayList;
+import java.util.*;
+
+import net.jqwik.api.*;
+
+import static java.util.Arrays.*;
+import static org.assertj.core.api.Assertions.*;
+
+@Label("Collections.reverse(List)")
+class ListReverseTests {
+
+	@Example
+	@Label("empty list")
+	void emptyList() {
+		List<Integer> aList = Collections.emptyList();
+		assertThat(reverse(aList)).isEmpty();
+	}
+
+	@Example
+	@Label("one element")
+	void oneElement() {
+		List<Integer> aList = Collections.singletonList(1);
+		assertThat(reverse(aList)).containsExactly(1);
+	}
+
+	@Example
+	@Label("many elements")
+	void manyElements() {
+		List<Integer> aList = asList(1, 2, 3, 4, 5, 6);
+		assertThat(reverse(aList)).containsExactly(6, 5, 4, 3, 2, 1);
+	}
+
+	@Example
+	@Label("duplicate elements")
+	void duplicateElements() {
+		List<Integer> aList = asList(1, 2, 2, 4, 6, 6);
+		assertThat(reverse(aList)).containsExactly(6, 6, 4, 2, 2, 1);
+	}
+
+	private <T> List<T> reverse(List<T> original) {
+		List<T> clone = new ArrayList<>(original);
+		// Should produce failing properties:
+		// List<T> clone = new ArrayList<>(new HashSet<>(original));
+		Collections.reverse(clone);
+		return clone;
+	}
+
+}
