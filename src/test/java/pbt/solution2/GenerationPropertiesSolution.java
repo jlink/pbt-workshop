e342e96d42c7ef7a627fd9da3cd827d4ea107860
Johannes Link <business@johanneslink.net>
2018-10-25 14:20:27 +0200

    Added part of solution for exercise 2

--- src/test/java/pbt/solution2/GenerationPropertiesSolution.java
+++ src/test/java/pbt/solution2/GenerationPropertiesSolution.java
@@ -1,29 +1,47 @@
-package pbt.exercise2;
+package pbt.solution2;
 
 import org.assertj.core.api.*;
+import pbt.exercise2.*;
 
 import net.jqwik.api.*;
 import net.jqwik.api.Tuple.*;
+import net.jqwik.api.arbitraries.*;
+import net.jqwik.api.constraints.*;
 
 import static org.assertj.core.api.Assertions.*;
 
-class GenerationProperties {
+class GenerationPropertiesSolution {
 
-	//@Property
+	@Property
 	@Label("German zipcode is valid")
-	void germanZipcodeIsValid(@ForAll String germanZipcode) {
+	void germanZipcodeIsValid(@ForAll @StringLength(5) @CharRange(from = '0', to = '9') String germanZipcode) {
 		assertThat(germanZipcode).hasSize(5);
 		isValidGermanZipCode(germanZipcode);
 	}
 
-	//@Property
+	@Property
+	@Report({Reporting.GENERATED})
 	@Label("String.substring() never throws exception")
-	void stringSubstringWorks(@ForAll Tuple3<String, Integer, Integer> substringParams) {
+	void stringSubstringWorks(@ForAll("substringParams") Tuple3<String, Integer, Integer> substringParams) {
 		String initialString = substringParams.get1();
 		int beginIndex = substringParams.get2();
 		int endIndex = substringParams.get3();
 
 		assertThatCode(() -> initialString.substring(beginIndex, endIndex)).doesNotThrowAnyException();
+	}
+
+	@Provide
+	Arbitrary<Tuple3<String, Integer, Integer>> substringParams() {
+		Arbitrary<String> string = Arbitraries.strings().alpha().ofMinLength(1).ofMaxLength(50);
+		return string.flatMap(s -> {
+			Arbitrary<Integer> begin = Arbitraries.integers().between(0, s.length() - 1);
+			return begin.flatMap(
+					b -> {
+						Arbitrary<Integer> end = Arbitraries.integers().between(b, s.length() - 1);
+						return end.map(e -> Tuple.of(s, b, e));
+					}
+			);
+		});
 	}
 
 	//@Property
