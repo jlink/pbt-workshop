72ad1a559cce5a532d5c2abbdd5641f387767ae2
Johannes Link <business@johanneslink.net>
2018-10-29 11:04:42 +0100

    Added two examples

---
+++ src/test/java/pbt/examples/FizzBuzzExamples.java
@@ -0,0 +1,54 @@
+package pbt.examples;
+
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.*;
+
+@Label("Calling fizzBuzz with...")
+class FizzBuzzExamples {
+
+	@Property
+	@Label("multiple of 3 contains 'Fizz'")
+	boolean multiple3ContainsFizz(@ForAll("multipleOf3") int anInt) {
+		return fizzBuzz(anInt).contains("Fizz");
+	}
+
+	@Provide
+	Arbitrary<Integer> multipleOf3() {
+		return Arbitraries.integers().between(1, 33).map(i -> i * 3);
+	}
+
+	@Property
+	@Label("multiple of 5 contains 'Buzz'")
+	boolean divisibleBy5ContainsBuzz(@ForAll @IntRange(min = 1, max = 100) int anInt) {
+		Assume.that(anInt % 5 == 0);
+		return fizzBuzz(anInt).contains("Buzz");
+	}
+
+	@Property
+	@Label("number that is not a multiple of 3 nor 5 returns the number itself")
+	boolean indivisiblesReturnThemselves(@ForAll("notMultiple") int anInt) {
+		return fizzBuzz(anInt).equals(Integer.toString(anInt));
+	}
+
+	@Provide
+	Arbitrary<Integer> notMultiple() {
+		return Arbitraries.integers().between(1, 100)
+						  .filter(i -> i % 3 != 0 && i % 5 != 0);
+	}
+
+	@Property(maxDiscardRatio = 20)
+	@Label("a multiple of 3 and 5 returns 'FizzBuzz'")
+	boolean multipleOf3and5ReturnFizzBuzz(@ForAll @IntRange(min = 1, max = 100) int anInt) {
+		Assume.that(anInt % 3 == 0);
+		Assume.that(anInt % 5 == 0);
+		return fizzBuzz(anInt).equals("FizzBuzz");
+	}
+
+	private String fizzBuzz(int anInt) {
+		if (anInt % 15 == 0) return "FizzBuzz";
+		if (anInt % 3 == 0) return "Fizz";
+		if (anInt % 5 == 0) return "Buzz";
+		return Integer.toString(anInt);
+	}
+
+}
---
+++ src/test/java/pbt/examples/IntegratedShrinkingExamples.java
@@ -0,0 +1,32 @@
+package pbt.examples;
+
+import net.jqwik.api.*;
+
+class IntegratedShrinkingExamples {
+
+	@Property(shrinking = ShrinkingMode.OFF)
+	@Report(Reporting.FALSIFIED)
+	boolean shrinkingCanBeComplicated(
+			@ForAll("first") String first,
+			@ForAll("second") String second
+	) {
+		String aString = first + second;
+		return aString.length() < 4 || aString.length() > 5;
+	}
+
+	@Provide
+	Arbitrary<String> first() {
+		return Arbitraries.strings()
+						  .withCharRange('a', 'z')
+						  .ofMinLength(1).ofMaxLength(10)
+						  .filter(string -> string.endsWith("h"));
+	}
+
+	@Provide
+	Arbitrary<String> second() {
+		return Arbitraries.strings()
+						  .withCharRange('0', '9')
+						  .ofMinLength(0).ofMaxLength(10)
+						  .filter(string -> string.length() >= 1);
+	}
+}
