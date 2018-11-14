1152463af4f0c33e278d3d1e4f77f64c75c2a326
Johannes Link <business@johanneslink.net>
2018-11-14 20:28:55 +0100

    Spelling errors in variable name

--- src/test/java/pbt/solution2/ZipCodePropertiesSolution.java
+++ src/test/java/pbt/solution2/ZipCodePropertiesSolution.java
@@ -10,10 +10,10 @@ class ZipCodePropertiesSolution {
 
 	@Property
 	@Label("is valid")
-	void germanZipCodeIsValid(@ForAll @StringLength(5) @CharRange(from = '0', to = '9') String germanZipcode) {
-		Assume.that(!germanZipcode.matches("00\\d+"));
-		assertThat(germanZipcode).hasSize(5);
-		isValidGermanZipCode(germanZipcode);
+	void germanZipCodeIsValid(@ForAll @StringLength(5) @CharRange(from = '0', to = '9') String germanZipCode) {
+		Assume.that(!germanZipCode.matches("00\\d+"));
+		assertThat(germanZipCode).hasSize(5);
+		isValidGermanZipCode(germanZipCode);
 	}
 
 	private void isValidGermanZipCode(@ForAll String germanZipcode) {
