6b9c6524dd6babf6b52fa6accef32d3a6f439049
Johannes Link <business@johanneslink.net>
2020-04-28 10:01:49 +0200

    Added MetamorphicPropertyExamples

---
+++ src/test/java/pbt/examples/MetamorphicPropertyExamples.java
@@ -0,0 +1,23 @@
+package pbt.examples;
+
+import java.util.*;
+
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.*;
+
+class MetamorphicPropertyExamples {
+
+	@Property
+	boolean summingAdditionalNumber(
+			@ForAll @Size(max = 100) List<@Positive Integer> addends,
+			@ForAll @Positive int x
+	) {
+		long sum = sumUp(addends);
+		addends.add(x);
+		return sum + x == sumUp(addends);
+	}
+
+	private long sumUp(final List<Integer> addends) {
+		return addends.stream().mapToLong(i -> i).sum();
+	}
+}
