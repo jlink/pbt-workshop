bdabea42188742a9c86c25a5efe79d1bbdd8e847
Johannes Link <business@johanneslink.net>
2020-11-17 08:28:46 +0100

    Set up Primes Kata

--- src/test/java/pbt/primes/Primes.java
+++ src/test/java/pbt/primes/Primes.java
@@ -5,20 +5,6 @@ import java.util.*;
 class Primes {
 
 	static List<Integer> factorize(int number) {
-		List<Integer> factors = new ArrayList<>();
-		int candidate = 2;
-		while (number >= candidate) {
-			while (number % candidate != 0) {
-				if (candidate > Math.sqrt(number)) {
-					candidate = number;
-				} else {
-					candidate++;
-				}
-			}
-			factors.add(candidate);
-			number /= candidate;
-		}
-
-		return factors;
+		return null;
 	}
 }
--- src/test/java/pbt/primes/PrimesProperties.java
+++ src/test/java/pbt/primes/PrimesProperties.java
@@ -1,12 +1,17 @@
 package pbt.primes;
 
+import java.util.*;
+
 import net.jqwik.api.*;
+
+import static org.assertj.core.api.Assertions.*;
 
 class PrimesProperties {
 
 	@Property
-	boolean fails() {
-		return false;
+	void just2() {
+		List<Integer> factors = Primes.factorize(2);
+		assertThat(factors).containsExactly(2);
 	}
 
 }
